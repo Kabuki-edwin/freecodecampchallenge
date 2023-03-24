5e91c2574c0d21377c722532533cbfaf205f02c0
cheezyfez <127417678+cheezyfez@users.noreply.github.com>
2023-03-24 10:11:55 +0300

    started learning javascript

---
+++ Javascript/basics.js
@@ -0,0 +1,16 @@
+// This is an in-line comment.
+/* This is a
+multi-line comment */
+var ourName;
+var myVar;
+myVar = 5;
+var myVar;
+myVar = 5;
+var myNum;
+myNum = myVar;
+
+let myVar = 87;
+myVar++;/*myVar=88*/
+
+let myVar = 87;
+myVar--; /*myVar=86*/

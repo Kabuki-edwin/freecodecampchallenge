83229e00f5fe843af31b4ca7061b361152e9ae6d
cheezyfez <127417678+cheezyfez@users.noreply.github.com>
2023-03-29 10:42:10 +0300

    adding a few changes

--- Javascript/basics.js
+++ Javascript/basics.js
@@ -1,16 +1,5 @@
 // This is an in-line comment.
 /* This is a
 multi-line comment */
-var ourName;
-var myVar;
-myVar = 5;
-var myVar;
-myVar = 5;
-var myNum;
-myNum = myVar;
-
-let myVar = 87;
-myVar++;/*myVar=88*/
-
-let myVar = 87;
-myVar--; /*myVar=86*/
+let example = "you got it right.", secondExample ="You got this too.", thirdExample="And thiss too";
+document.getElementById("demo4").innerHTML=example ;

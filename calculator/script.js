e04cbae05230af923126fa9a7ffa333de2a63e06
cheezyfez <127417678+cheezyfez@users.noreply.github.com>
2023-03-31 15:14:19 +0300

    added equal button

--- calculator/script.js
+++ calculator/script.js
@@ -1,11 +1,25 @@
-(function(){
+(function() {
 
-    let screen = document.querySelector('screen');
+    let screen = document.querySelector('.screen');
     let buttons = document.querySelectorAll('.btn');
-    let equal= document.querySelector('.btn-green');
-    let clear = document.querySelector('.btn-red');
+    let equal = document.querySelector('.btn-equal');
+    let clear = document.querySelector('.btn-clear');
 
     buttons.forEach(function (button) {
-        button.addEventListener('click',)
+        button.addEventListener('click',function (e) {
+            let value = e.target.dataset.num;
+            screen.value += value;
+        })
+    });
+
+    equal .addEventListener('click', (e) => {
+            if(screen.value === '') {
+                screen.value = "";
+            } else {
+                let answer = eval(screen.value);
+                screen.value = answer;
+            }
+        })
+    
     })
 })();

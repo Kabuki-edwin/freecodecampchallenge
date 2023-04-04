aa9bfc6f4bedd67f31db52670edc8ddb14a5474c
cheezyfez <edunashfez@gmail.com>
2023-04-04 14:28:28 +0300

    default option

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -101,3 +101,25 @@ function caseInSwitch(val) {
   }
   
   console.log(caseInSwitch(1));
+
+  //default option in switch statements
+  function switchOfStuff(val) {
+    let answer = "";
+  switch(val) {
+    case "a":
+     answer = "apple";
+     break;
+    case "b":
+     answer = "bird";
+     break;
+    case "c":
+     answer = "cat";
+     break;
+    default:
+    answer = "stuff";
+    break;
+  }
+    return answer;
+  }
+  
+  console.log(switchOfStuff(1));

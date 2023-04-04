4ce12e83b6d36ec95b95da42e84c8c1344e2ba6f
cheezyfez <edunashfez@gmail.com>
2023-04-04 11:53:10 +0300

    toogled class active

--- search widget/script .js
+++ search widget/script .js
@@ -1,5 +1,8 @@
-const  search = document.querySelector.apply('.search');
-const  btn = document.querySelector.apply('.btn');
-const  input = document.querySelector.apply('.input');
+const  search = document.querySelector('.search');
+const  btn = document.querySelector('.btn');
+const  input = document.querySelector('.input');
 
-btn.addEventListener('click')
+btn.addEventListener('click', () => {
+    search.classList.toggle('active')
+    input.focus()
+})

de4611061491fbc71db45a234f9fd21201f8e34b
cheezyfez <edunashfez@gmail.com>
2023-04-05 13:27:42 +0300

    finished todays project

--- blurry/loading/page/script.js
+++ blurry/loading/page/script.js
@@ -12,4 +12,10 @@ function blurring() {
         clearInterval(int)
     }
     loadingText.innerText = `${load}%`
+    loadingText.style.opacity =  scale(load, 0, 100, 1, 0)
+    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+}
+
+const scale = (number, inMin, inMax, outMin, outMax) => {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
 }

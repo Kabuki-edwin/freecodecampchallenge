2f9b421c73387ed3f508d19c7e37a2403fa644cd
cheezyfez <edunashfez@gmail.com>
2023-04-05 11:27:53 +0300

    added a condition

--- blurry/loading/page/script.js
+++ blurry/loading/page/script.js
@@ -1,3 +1,14 @@
 const loadingText = document.querySelector('.loading-text')
 const bg = document.querySelector('.bg')
 
+let int = setInterval(blurring, 30)
+
+let load = 0;
+
+function blurring() {
+    load++
+
+    if (load > 99) {
+        clearInterval(int)
+    }
+}

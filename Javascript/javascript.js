9fed32b228f4e2bd48948f8826d72f4039c9c4ef
cheezyfez <edunashfez@gmail.com>
2023-04-06 11:41:35 +0300

    Nesting for loops

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -475,3 +475,17 @@ let total = 0;
 for (let i = 0;i < myArr.length;i++){
   total += myArr[i]
 }
+
+
+//Nesting for loops
+function multiplyAll(arr) {
+  let product = 1;
+for (var i=0; i < arr.length; i++) {
+    for (var j=0; j < arr[i].length; j++) {
+      product *= arr[i][j];
+    }
+  }
+  return product;
+}
+
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);

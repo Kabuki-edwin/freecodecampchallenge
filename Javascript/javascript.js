cbce55184bfd9f0d2026f86b83e5c6737e4403f4
cheezyfez <edunashfez@gmail.com>
2023-04-11 12:24:55 +0300

    Destructuring via  rest element

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -696,3 +696,12 @@ const {today:{low:lowToday ,high:highTtoday}} = LOCAL_FORECAST;
 let a = 8, b = 6;
 
 [a, b] = [b, a]
+
+//Destructuring via  rest element
+function removeFirstTwo(list) {
+  const [a, b, ...shorterList] = list;
+  return shorterList;
+}
+
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const sourceWithoutFirstTwo = removeFirstTwo(source);

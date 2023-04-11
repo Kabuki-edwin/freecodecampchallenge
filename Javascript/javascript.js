32b5e2d1836b7514490cab32476957675f97d34c
cheezyfez <edunashfez@gmail.com>
2023-04-11 10:03:05 +0300

    Mutating an array declared with const

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -611,3 +611,16 @@ function checkScope() {
   console.log('Function scope i is: ', i);
   return i;
 }
+
+//Mutating an array declared with const
+const s = [5, 7, 2];
+function editInPlace() {
+  // Only change code below this line
+
+  // Using s = [2, 5, 7] would be invalid
+s[0] = 2;
+s[1] = 5;
+s[2] = 7;
+  // Only change code above this line
+}
+editInPlace();

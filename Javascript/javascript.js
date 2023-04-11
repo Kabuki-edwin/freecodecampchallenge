bb21653b7e552bb09df3e01d98c464233294d1b6
cheezyfez <edunashfez@gmail.com>
2023-04-11 12:46:07 +0300

    Write Concise Object Literal Declarations Using Object Property Shorthand

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -734,3 +734,8 @@ function makeList(arr) {
 }
 
 const failuresList = makeList(result.failure);
+
+//Write Concise Object Literal Declarations Using Object Property Shorthand
+const createPerson = (name, age, gender) => {
+return {name, age, gender};
+};

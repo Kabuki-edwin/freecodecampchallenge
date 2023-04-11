261d17256b9bc6e3a5097e39cd063b9157571505
cheezyfez <edunashfez@gmail.com>
2023-04-11 12:40:40 +0300

    Create Strings using Template Literals

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -717,3 +717,20 @@ const stats = {
 }
 
 const half = ({max, min}) => (max + min) / 2.0; 
+
+//Create Strings using Template Literals
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"]
+};
+function makeList(arr) {
+  const failureItems = [];
+  for (let i = 0; i < arr.length; i++) {
+    failureItems.push(`<li class="text-warning">${arr[i]}</li>`);
+  }
+
+  return failureItems;
+}
+
+const failuresList = makeList(result.failure);

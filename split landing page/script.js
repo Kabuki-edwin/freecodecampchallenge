c333b77917b58310108fdfd3a1d6f3a1afce0e7d
cheezyfez <edunashfez@gmail.com>
2023-04-12 09:24:49 +0300

    added event listener

--- split landing page/script.js
+++ split landing page/script.js
@@ -1,3 +1,5 @@
 const left = document.querySelector('.left')
 const right= document.querySelector('.right')
 const container = document.querySelector('.container')
+
+left.addEventListener('mouseenter', () => container.classList.add('hover-left'))

663be71e1f34df462c8767a29c61db97a163303f
cheezyfez <edunashfez@gmail.com>
2023-04-12 12:40:45 +0300

    correcting a major error

--- loginform/script.js
+++ loginform/script.js
@@ -1,4 +1,4 @@
-const labels = document.querySelectorAll ('.form-control')
+const labels = document.querySelectorAll ('.form-control label')
 
 labels.forEach(label => {
     label.innerHTML = label.innerText

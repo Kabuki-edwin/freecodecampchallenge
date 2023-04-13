40194dc25321f6237dde60ca9bfc98a73977adf2
cheezyfez <edunashfez@gmail.com>
2023-04-13 16:18:59 +0300

    created a function

--- dad jokes/script.js
+++ dad jokes/script.js
@@ -1,2 +1,12 @@
 const jokeEl = document.getElementById('joke')
 const jokeBtn = document.getElementById('jokeBtn')
+
+generatJoke()
+
+function generatJoke() {
+    fetch ('https://icanhazdadjoke.com', {
+        headers: {
+            'Accept':'application/json'
+        }
+    })
+}

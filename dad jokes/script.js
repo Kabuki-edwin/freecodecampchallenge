17cd0b694dec6493cf840ad7bff10177d4bcf576
cheezyfez <edunashfez@gmail.com>
2023-04-14 11:19:31 +0300

    experienced some errors

--- dad jokes/script.js
+++ dad jokes/script.js
@@ -1,20 +1,20 @@
 const jokeEl = document.getElementById('joke')
 const jokeBtn = document.getElementById('jokeBtn')
 
-jokeBtn.addEventListener('click',generatJoke)
+jokeBtn.addEventListener('click', generateJoke)
 
 generateJoke()
 
 function generateJoke() {
     const config = {
         headers: {
-            Accept:'application/json',
+            Accept: 'application/json',
         },
     }
 
-    fetch ('https://icanhazdadjoke.com', config)
-    .then((res) => res.json())
-    .then((data) => {
+    fetch('https://icanhazdadjoke.com', config)
+     .then((res) => res.json())
+     .then((data) => {
         jokeEl.innerHTML = data.joke 
     })
 }

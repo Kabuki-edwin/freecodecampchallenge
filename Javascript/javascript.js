99dc3b10832199bb00122f5aa08af7a9a51c5423
cheezyfez <edunashfez@gmail.com>
2023-04-14 11:48:08 +0300

    Using type of to check the type of variable

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -1026,3 +1026,10 @@ console.log(sumAB);
 let output = "Get this to show once in the freeCodeCamp console and not at all in the browser console";
 console.log(output)
 console.clear()
+
+//Using type of to check the type of variable
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+console.log(typeof seven)
+console.log(typeof three)

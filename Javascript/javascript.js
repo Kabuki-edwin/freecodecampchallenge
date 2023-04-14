9c9a46339a01d022ffbe9e72e768955561740ea3
cheezyfez <edunashfez@gmail.com>
2023-04-14 12:02:19 +0300

    Catch Unclosed Parentheses, Brackets, Braces and Quotes

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -1039,3 +1039,8 @@ let receivables = 10;
 let payables = 8;
 let netWorkingCapital = receivables - payables;
 console.log(`Net working capital is: ${netWorkingCapital}`);
+
+//Catch Unclosed Parentheses, Brackets, Braces and Quotes
+let myArrray = [1, 2, 3];
+let arraySum = myArray.reduce((previous, current) =>  previous + current);
+console.log(`Sum of array values is: ${arraySum}`);

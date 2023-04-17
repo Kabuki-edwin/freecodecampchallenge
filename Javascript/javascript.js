d01a8b7ca5eff69bdbc35a288b2abb92b1ff4223
cheezyfez <edunashfez@gmail.com>
2023-04-17 12:44:00 +0300

    removing items using splice

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -1146,3 +1146,10 @@ function popShift(arr) {
 }
 
 console.log(popShift(['challenge', 'is', 'not', 'complete']));
+
+//Removing items using splice
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+
+arr.splice(1,4)
+
+console.log(arr);
